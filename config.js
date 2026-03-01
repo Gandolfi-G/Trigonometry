20cc8fbcf210dd73d9637b162feea4e9952d5ee7
Gandolfi-G <91029843+Gandolfi-G@users.noreply.github.com>
2026-03-01 18:00:22 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+window.SOLIDES_CONFIG = {
+  "teacherCode": "COUCOU26",
+  "releaseDates": {
+    "01-vocabulaire": "2026-03-01",
+    "02-polyedres": "2026-03-02",
+    "03-patrons": "2026-03-03",
+    "04-volume": "2026-03-04",
+    "05-prismes": "2026-03-05",
+    "evaluation": "2026-03-06"
+  },
+  "moduleLabels": {
+    "01-vocabulaire": "Module 1 : Vocabulaire des solides",
+    "02-polyedres": "Module 2 : Polyedres",
+    "03-patrons": "Module 3 : Patrons",
+    "04-volume": "Module 4 : Volume",
+    "05-prismes": "Module 5 : Prismes droits",
+    "evaluation": "Evaluation finale"
+  }
+};
